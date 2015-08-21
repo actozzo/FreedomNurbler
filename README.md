9a66e4770902628ff6fc50d9701d06d55fdd3431
atozter <actozzo@gmail.com>
2015-08-20 22:18:10 -0400

    Finalized beta version of code. To-do: enable random text files to be nurbled

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # FreedomNurbler
 Inspired by this SMBC comic: http://www.smbc-comics.com/?id=2779
 
+This is very much tongue-in-cheek, perhaps even, tongue-through-cheek.
+
 Original nurbler replaces everything but nouns to 'nurble'. 
 
-Freedom nurbler replaces random verbs (set some threshold like 10%) with a patriotic verb such as 'liberate' or 'defend'. Define a list of verbs in another text file.
+Freedom nurbler replaces verbs with a patriotic verb such as 'liberate' or 'defend'. Define a list of verbs in another text file. Originally, verbs had a ~10% chance of being replaced. Then I increased the threshold to 40% and it was still funny. So I've removed that threshold completely and every verb will be replaced with a verb from the 'chosen' list.
+
+
--- freeNurb.py
+++ freeNurb.py
@@ -7,27 +7,35 @@ Freedom Nurbler
 
 """
 import nltk
+import random
 
 
 okPOS = ['NN', 'NNP', 'NNS'] #allowable nouns of speech
 verbs = ['VBD', 'VBP', 'VB', 'VBZ'] #allowable verbs
 punc = [',', '.', '!', '?'] #don't nurble punctuation
 
-def nurbWord(taggedWord):
+def nurbWord(taggedWord, verbList):
     if taggedWord[1] in okPOS + punc:
         return taggedWord[0].upper()
     elif taggedWord[1] in verbs:
-        if rand() <= 0.1:
+        return verbList[random.randint(0, len(verbList)-1)].upper()
             # randomly choose verb from list
-            return
     return 'nurble'
+
+def untok(words):
+    #copied from http://languagelog.ldc.upenn.edu/nll/?p=4282
+    return "".join(words[0:1]+ [w if w in punc else " " + w for w in words[1:]])
 
 f = open('sotu2012.txt')
 sotu = f.read()
 f.close()
-sentences = nltk.tokenize(sotu)
+sentences = nltk.tokenize.sent_tokenize(sotu)
 taggedSentences = [nltk.pos_tag(nltk.word_tokenize(s)) for s in sentences]
 
 v = open('freedomverbs.txt')
 verbList = v.read()
+verbList = nltk.word_tokenize(verbList)
 v.close()
+
+nurbled = open('nurbled.txt', 'w')
+nurbled.write("".join(untok([nurbWord(w, verbList) for w in s]) for s in taggedSentences))
